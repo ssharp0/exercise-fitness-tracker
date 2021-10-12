4e231c61d97918c5369b9176140eb8b52a557752
SHARP <ssharp9999@gmail.com>
2021-10-11 21:51:59 -0700

    db

--- db/index.js
+++ db/index.js
@@ -1,1 +1,5 @@
+// require mongoose
+const mongoose = require('mongoose')
 
+// export syncDB (connect to MONGODB URI [deployed on Heroku] or local host)
+module.exports = async function syncDB() { await mongoose.connect(process.env.MONGODB_URI || ) }
