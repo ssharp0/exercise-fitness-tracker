b3983fcc850c6f8554d97f541cab2f3e449fa988
SHARP <ssharp9999@gmail.com>
2021-10-12 17:02:02 -0700

    file start

---
+++ routes/apiWorkoutRoutes.js
@@ -0,0 +1,10 @@
+// require express router
+const router = require('express').Router();
+
+// require Workout model
+const { Workout } = require('../models')
+
+
+
+// export the router
+module.exports = router
