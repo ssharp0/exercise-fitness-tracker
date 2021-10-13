cc09e3eca9cb3d7d7e24e6e4a2e756c0def6bd64
SHARP <ssharp9999@gmail.com>
2021-10-12 18:13:24 -0700

    index routes to include api views and html

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
+// // require express router
+const router  = require('express').Router()
 
+// // prefix api to workout routes
+router.use('/api', require('./apiWorkoutRoutes.js'))
+router.use(require('./viewRoutes.js'))
+
+// export the router
+module.exports = router
