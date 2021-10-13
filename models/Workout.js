acde48582c7eae7505c54362f0aebe2e1f73b7ee
SHARP <ssharp9999@gmail.com>
2021-10-12 18:11:44 -0700

    updated with virtual for total

--- models/Workout.js
+++ models/Workout.js
@@ -35,8 +35,24 @@ const Workout = new Schema({
    type: Number,
    default: 0
   }
- }]
+ }],
+ // totalDuration: {
+ //  type: Number,
+ //  default: 0
+ // }
 })
 
+// https://mongoosejs.com/docs/api.html#schema_Schema-virtuals
+// create a virtual type totalDuration to use (sum the exercises in a workout) - this will be used for line chart
+Workout.set('toObject', { virtuals: true })
+Workout.set('toJSON', { virtuals: true })
 
+// set the virtual totalDuration - for each exercise (in the workouts) the total duration is equal to the addition assignment operator (i.e. sum of all the exercise durations in a workout) - then return the total duration
+Workout.virtual('totalDuration').get(function () {
+  let totalDuration = 0
+  this.exercises.forEach(exercise => { totalDuration += exercise.duration })
+  return totalDuration
+ })
+
+ // export the workout model
 module.exports = model('Workout', Workout )
